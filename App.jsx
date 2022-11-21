0d338fa1dd8497924d2543f7b1f2207b1c2494e8
Felipe-OT <felipe.rm@outlook.com.br>
2022-11-21 12:33:42 -0300

    instalação de dependencias

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/header/Header";
+import JobsContainer from "./components/jobsContainer/JobsContainer";
+
+function App() {
+  return (
+    <div className="h-screen flex flex-col overflow-y-hidden">
+      <Header />
+      <main className="flex h-[calc(100vh-96px)] pt-10">
+        <JobsContainer />
+      </main>
+    </div>
+  );
+}
+
+export default App;
