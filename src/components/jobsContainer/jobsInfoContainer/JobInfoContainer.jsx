87290dd7466530d86ac08b55179076906761fbc9
Felipe-OT <felipe.rm@outlook.com.br>
2022-11-23 15:36:48 -0300

    Adicionado CTA fixo onScroll no quadro de info

--- src/components/jobsContainer/jobsInfoContainer/JobInfoContainer.jsx
+++ src/components/jobsContainer/jobsInfoContainer/JobInfoContainer.jsx
@@ -1,16 +1,33 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import JobContext from "../../../context/jobContext";
 
 const JobInfoContainer = () => {
   const { selectedJob } = useContext(JobContext);
-  const [showInfoScroll, setShowInfoScroll] = useState(true);
+  const scrollRef = useRef();
+  const [showInfoScroll, setShowInfoScroll] = useState(false);
+
+  const handleScroll = useCallback((e) => {
+    if (e.currentTarget.scrollTop > 135) {
+      setShowInfoScroll(true)
+    } else {
+      setShowInfoScroll(false)
+    }
+    
+
+  }, []);
 
   return (
     <div className="hidden relative sm:w-full sm:flex lg:mt-[38px] xl:w-2/3 flex-col gap-3 overflow-y-hidden ">
       <div
         className={`${
           showInfoScroll ? "opacity-100" : "opacity-0"
-        } absolute top-0 left-0 flex flex-row justify-between w-full px-8 py-4 bg-slate-500 border-b rounded-t-md z-50`}
+        } absolute top-0 left-0 flex flex-row justify-between w-[calc(100%-20px)] px-8 py-4 bg-white border-b rounded-t-md z-50`}
       >
         <div>
           <h1 className="text-xl font-semibold">{selectedJob.title}</h1>
@@ -21,13 +38,16 @@ const JobInfoContainer = () => {
             <p className="text-[#63B4FF] font-medium">{selectedJob.location}</p>
           </div>
         </div>
-        <div>
-          <button className="bg-[#4D82AB] hover:bg-[#53a0e7] transition-all text-white font-semibold  rounded-xl p-3">
+        <div className="flex flex-shrink-0 max-h-10 items-center">
+          <button className="bg-[#E4C392] hover:bg-[#eecb98] transition-all text-white font-semibold rounded-xl px-3 py-2">
             Candidatar-se
           </button>
         </div>
       </div>
-      <div className=" relative h-full overflow-y-scroll sm:flex flex-col gap-3 bg-white w-full rounded-md p-8">
+      <div
+        onScroll={handleScroll}
+        className=" relative h-full overflow-y-scroll sm:flex flex-col gap-3 bg-white w-full rounded-md p-8"
+      >
         <div className="flex flex-col">
           <div className="mb-8">
             <p className="text-[#63B4FF] font-medium">
