9743c14739f954499c64019992495f72c5bf91d7
Felipe-OT <felipe.rm@outlook.com.br>
2022-11-23 00:40:30 -0300

    Adicionado paginação na lista de jobs

--- src/components/jobsContainer/jobsList/JobsList.jsx
+++ src/components/jobsContainer/jobsList/JobsList.jsx
@@ -1,13 +1,21 @@
 import React, { useEffect, useRef, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 import JobCard from "./jobCard/JobCard";
+import Pagination from "./pagination/Pagination";
 
 const JobsList = () => {
-    const messagesEndRef = useRef(null);
-    const [jobsData, setJobsData] = useState([]);
+  const messagesEndRef = useRef(null);
+  const [jobsData, setJobsData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
-
+  // Variáveis de paginação
+  const [postsPerPage] = useState(16);
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const [filteredJobs, setFilteredJobs] = useState([]);
+  const currentJobs = jobsData.slice(indexOfFirstPost, indexOfLastPost);
+  const currentFiltered = filteredJobs.slice(indexOfFirstPost, indexOfLastPost);
 
   useEffect(() => {
     // Coleta os dados da api
@@ -18,7 +26,6 @@ const JobsList = () => {
         );
         console.log(response.data);
         setJobsData(response.data.data);
-
       } catch (e) {
         console.log(e);
       }
@@ -30,6 +37,28 @@ const JobsList = () => {
     return () => (unmounted = true);
   }, []);
 
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const previousPage = () => {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const nextPage = () => {
+    if (filteredJobs.length > 0) {
+      if (currentPage !== Math.ceil(filteredJobs.length / postsPerPage)) {
+        setCurrentPage(currentPage + 1);
+      }
+    } else {
+      if (currentPage !== Math.ceil(jobsData.length / postsPerPage)) {
+        setCurrentPage(currentPage + 1);
+      }
+    }
+  };
+
   return (
     <div className="flex flex-col w-full sm:w-[70%] lg:pt-[38px] xl:w-1/3 overflow-y-hidden">
       <div className="text-center bg-[#F8D5A2] py-1 text-white font-medium rounded-t-[6px]">
@@ -40,9 +69,18 @@ const JobsList = () => {
         className="flex flex-col flex-grow  overflow-y-scroll overflow-x-hidden"
       >
         <div className="flex flex-col lg:gap-y-3">
-          {jobsData.length > 0
-            ? jobsData.map((job, i) => <JobCard key={i} {...job} />)
-            : jobsData.map((job, i) => <JobCard key={i} {...job} />)}
+          {jobsData.length > 0 &&
+            currentJobs.map((job, i) => <JobCard key={i} {...job} />)}
+        </div>
+        <div>
+          <Pagination
+            postsPerPage={postsPerPage}
+            totalPosts={jobsData.length}
+            currentPage={currentPage}
+            paginate={paginate}
+            previousPage={previousPage}
+            nextPage={nextPage}
+          />
         </div>
       </div>
     </div>
---
+++ src/components/jobsContainer/jobsList/pagination/Pagination.jsx
@@ -0,0 +1,97 @@
+const Pagination = ({
+  postsPerPage,
+  totalPosts,
+  paginate,
+  currentPage,
+  previousPage,
+  nextPage,
+}) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  console.log(paginate)
+
+  return (
+    <div  className="py-5 bg-white">
+      <ul className="flex flex-row justify-center items-center gap-4">
+        <li className="cursor-pointer" onClick={previousPage}>
+          <svg
+            width="23"
+            height="22"
+            viewBox="0 0 23 22"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M11.5 21C17.0228 21 21.5 16.5228 21.5 11C21.5 5.47715 17.0228 1 11.5 1C5.97715 1 1.5 5.47715 1.5 11C1.5 16.5228 5.97715 21 11.5 21Z"
+              stroke="#DADADA"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M11.5 7L7.5 11L11.5 15"
+              stroke="#DADADA"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M15.5 11H7.5"
+              stroke="#DADADA"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </li>
+        <div className="flex flex-row justify-center gap-2">
+        {pageNumbers.map((number, i) => (
+          <div key={i} className={`w-[25px] h-[25px] flex rounded-full cursor-pointer ${currentPage == i + 1 ? 'bg-[#F8D5A2] text-white' : ''}`} onClick={() => paginate(number)}>
+              <li className="mx-auto" key={number} >
+                {number}
+              </li>
+          </div>
+        ))}
+        </div>
+        
+        <li className="cursor-pointer" onClick={nextPage}>
+          <svg
+            width="23"
+            height="22"
+            viewBox="0 0 23 22"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M11.5 21C17.0228 21 21.5 16.5228 21.5 11C21.5 5.47715 17.0228 1 11.5 1C5.97715 1 1.5 5.47715 1.5 11C1.5 16.5228 5.97715 21 11.5 21Z"
+              stroke="#DADADA"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M11.5 15L15.5 11L11.5 7"
+              stroke="#DADADA"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M7.5 11H15.5"
+              stroke="#DADADA"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Pagination;
